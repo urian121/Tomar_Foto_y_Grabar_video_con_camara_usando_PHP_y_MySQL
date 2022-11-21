b7c0571739ab3edc7835e3b6cfad3cc542f7980f
urian <urian1213viera@gmail.com>
2022-11-20 20:22:58 -0500

    subiendo bd

--- BD/archivos.sql
+++ BD/archivos.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:3306
--- Tiempo de generación: 27-03-2022 a las 15:44:12
--- Versión del servidor: 10.3.34-MariaDB-cll-lve
--- Versión de PHP: 7.3.33
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-11-2022 a las 02:22:03
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sgbcolom_demos`
+-- Base de datos: `foto_video`
 --
 
 -- --------------------------------------------------------
@@ -31,19 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `archivos` (
   `id` int(10) NOT NULL,
   `nameFile` text DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `dateFile` varchar(30) NOT NULL
+  `dateFile` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `archivos`
 --
 
-INSERT INTO `archivos` (`id`, `nameFile`, `email`, `dateFile`) VALUES
-(12, '6240c89adc33c.png', NULL, '27-03-2022 15:27:06 PM'),
-(13, '6240c8a28a220.png', 'programadorphp2017@gmail.com', '27-03-2022 15:27:14 PM'),
-(14, '6240cb809766f.png', NULL, '27-03-2022 15:39:28 PM'),
-(15, '6240cb87780c5.png', 'urian1213viera@gmail.com', '27-03-2022 15:39:35 PM');
+INSERT INTO `archivos` (`id`, `nameFile`, `dateFile`) VALUES
+(1, '637ad13d3346b.png', '20-11-2022 20:15:41 PM'),
+(2, '637ad1565e493.png', '20-11-2022 20:16:06 PM'),
+(3, 'd765ed9571d71dd.mp4', '20-11-2022 20:16:28 PM'),
+(4, '637ad1ae8c74e.png', '20-11-2022 20:17:34 PM');
 
 --
 -- Índices para tablas volcadas
@@ -63,7 +61,7 @@ ALTER TABLE `archivos`
 -- AUTO_INCREMENT de la tabla `archivos`
 --
 ALTER TABLE `archivos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
